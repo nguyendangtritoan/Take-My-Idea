b2b76230d8ba94aa9a1d0a454b32921709606e22
Nguyen Dang Toan <nguyendangtritoan2305@gmail.com>
2020-04-03 13:31:53 +0300

    Create project

---
+++ src/main/java/com/example/reddit_rep/RedditRepApplication.java
@@ -0,0 +1,13 @@
+package com.example.reddit_rep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditRepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditRepApplication.class, args);
+    }
+
+}
