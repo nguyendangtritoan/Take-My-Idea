ffcae7e4ebcd4ac28a71ea1901b5a510865e2e6b
Nguyen Dang Toan <nguyendangtritoan2305@gmail.com>
2021-01-18 18:07:00 +0200

    Adding Authority and UserDetailsServiceImpl - SpringSecurity

--- src/main/java/com/example/reddit_rep/service/UserService.java
+++ src/main/java/com/example/reddit_rep/service/UserService.java
@@ -1,14 +1,39 @@
 package com.example.reddit_rep.service;
 
 import com.example.reddit_rep.domain.User;
+import com.example.reddit_rep.repo.UserRepository;
+import com.example.reddit_rep.security.Authority;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
 
+    private UserRepository userRepo;
+    private PasswordEncoder passwordEncoder;
+
     public User save(User user) {
-        return null;
+        String encodePass = passwordEncoder.encode((user.getPassword()));
+        user.setPassword(encodePass);
+        System.out.println(user.getPassword());
+        Authority authority = new Authority();
+        authority.setAuthority("ROLE_USER");
+        authority.setUser(user);
+        user.getAuthoritySet().add(authority);
+
+        return userRepo.save(user);
+    }
+
+    @Autowired
+    public void setUserRepo(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
 
 }
--- src/main/java/com/example/reddit_rep/web/LoginController.java
+++ src/main/java/com/example/reddit_rep/web/LoginController.java
@@ -1,6 +1,8 @@
 package com.example.reddit_rep.web;
 
 import com.example.reddit_rep.domain.User;
+import com.example.reddit_rep.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+
+    private UserService userService;
 
     @GetMapping("/login")
     public String login() {
@@ -22,7 +26,14 @@ public class LoginController {
 
     @PostMapping("/register")
     public String registerPost(User user) {
-        System.out.println(user.toString());
-        return "redirect:/register";
+        User saveUser = userService.save(user);
+        System.out.println("None save user: " + user);
+        System.out.println("Saved user: " + saveUser);
+        return "redirect:/login";
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 }
